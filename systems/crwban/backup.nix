3028d5c6e48cab821033e56c918f2b5f93878b7f
Tom Usher <tom@tomusher.com>
2022-11-26 21:38:13 +0000

    Move crwban backup config to a derivation

--- systems/crwban/backup.nix
+++ systems/crwban/backup.nix
@@ -2,33 +2,16 @@
 
 let
   secrets = import ../../secrets/secrets.nix;
-  config = pkgs.substituteAll {
-    src = ./autorestic.yml;
-    account_id = secrets.backblaze_b2_account_id;
-    account_key = secrets.backblaze_b2_account_key;
-    password = secrets.restic_backups_password;
-
-  };
-  resticPackages = with pkgs; [ restic autorestic ];
+  backup-cron = import ./backup-cron/default.nix { inherit pkgs; };
+  backupPackages = with pkgs; [ backup-cron ];
 in {
-  environment.systemPackages = resticPackages; 
-
-  environment.etc = {
-    "autorestic/config.yml" = {
-      text = builtins.readFile config.out;
-      mode = "0440";
-    };
-    "autorestic/backup_excludes" = {
-      text = builtins.readFile ./backup_excludes;
-      mode = "0440";
-    };
-  };
+  environment.systemPackages = backupPackages; 
 
   systemd.services.autorestic = {
     serviceConfig.Type = "oneshot";
-    path = resticPackages;
+    path = backupPackages;
     script = ''
-      autorestic -c /etc/autorestic/config.yml cron
+      backup-cron
     '';
     startAt = "*-*-* *:0/5";
   };
